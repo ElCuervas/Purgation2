17a569d1cc29643279a1a3029510faedf12bbc8b
ElCuervas <sebastianaillapan017@gmail.com>
2023-11-28 21:42:01 -0300

    Codigo base

---
+++ desktop/src/com/purgation2/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.purgation2.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.purgation2.game.Setup;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Purgation2");
+		new Lwjgl3Application(new Setup(), config);
+	}
+}
