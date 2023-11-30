85df64b9e2733431bc14c88697d0a1f3d7a13b0c
ElCuervas <sebastianaillapan017@gmail.com>
2023-11-29 23:33:05 -0300

    implementacion de camara

--- core/src/com/purgation2/game/GameScreen.java
+++ core/src/com/purgation2/game/GameScreen.java
@@ -1,7 +1,21 @@
 package com.purgation2.game;
-import java.util.*;
-public class GameScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.ScreenUtils;
+import sun.jvm.hotspot.utilities.Assert;
 
+import javax.print.attribute.standard.MediaSize;
+import java.util.*;
+public class GameScreen implements Screen {
+
+	final Setup game;
+	OrthographicCamera camera;
+	Sprite mapa;
+	AssetManager asset;
 	Jugador player1;
 	Collection<Bala> balasJugador;
 	Collection<Bala> balasJefe;
@@ -10,6 +24,73 @@ public class GameScreen {
 	private double[] mejorasJugador;
 	private double[] mejorasEnemigo;
 	private double[] mejorasJefe;
+
+	public GameScreen(final Setup game){
+		this.game=game;
+		asset = new AssetManager();
+		asset.load("mapa.png", Texture.class);
+		asset.finishLoading();
+
+		mapa = new Sprite((Texture) asset.get("mapa.png"));
+		mapa.setPosition(0,0);
+		mapa.setSize(1000,1000);
+
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera(50*(h/w), 50);
+		camera.position.set(600, 480, 0); //x player, y player
+		camera.zoom=25;
+		camera.update();
+	}
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
+
+		game.batch.begin();
+		mapa.draw(game.batch);
+		game.batch.end();
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportWidth = 30f;
+		camera.viewportHeight = 30f * height/width;
+		camera.update();
+	}
+	@Override
+	public void show() {
+
+	}
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+
+
+
+
+
+
+
 
 	public void generarEnemigo() {
 		// TODO - implement GameScreen.generarEnemigo
--- core/src/com/purgation2/game/Menu.java
+++ core/src/com/purgation2/game/Menu.java
@@ -1,20 +1,65 @@
 package com.purgation2.game;
 
-public class Menu {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-	public void iniciarJuego() {
-		// TODO - implement Menu.iniciarJuego
-		throw new UnsupportedOperationException();
+
+public class Menu implements Screen {
+	final Setup game;
+
+	OrthographicCamera camera;
+	public Menu(final Setup game) {
+		this.game = game;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 	}
 
-	public void cerrarJuego() {
-		// TODO - implement Menu.cerrarJuego
-		throw new UnsupportedOperationException();
+	@Override
+	public void show() {
+
 	}
 
-	public void pausar() {
-		// TODO - implement Menu.pausar
-		throw new UnsupportedOperationException();
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
+
+		game.batch.begin();
+		game.font.draw(game.batch, "Purgation2", 100, 150);
+		game.font.draw(game.batch, "Has click para empezar", 100, 100);
+		game.batch.end();
+		if (Gdx.input.isTouched()) {
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
 	}
 
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
 }
