5f0e40ce888d195ca8da45b072c22e7c5b5916dd
ElCuervas <sebastianaillapan017@gmail.com>
2023-12-04 19:05:24 -0300

    Comienzo de prueba unitarias
    Implementacion de pruebas unitarias

---
+++ core/src/com/purgation2/game/EntidadTest.java
@@ -0,0 +1,15 @@
+package com.purgation2.game;
+class EntidadTest {
+
+    @org.junit.jupiter.api.Test
+    void setVidaMaxima() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDamage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setVelocidad() {
+    }
+}
